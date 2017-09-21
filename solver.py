89b1a8091921ecc0d922cde68cbc3bbd9a0a7d80
Stephen J Bush <muppetjones@gmail.com>
2017-09-20 21:23:05 -0500

    PATCH: First failing unittest

---
+++ solver.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Script for solving math. Yes, math. All of it.
+
+Usage:
+
+    solver.py 1 + 1
+
+"""
+
+
+def solve():
+    pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
